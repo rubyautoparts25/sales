e8323668caad22e597e845ce708df1ea44131e73
NaiadOfEpistemology <medapatitanvi@gmail.com>
2025-09-05 19:01:11 +0530

    sprint

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        billing: 'billing.html',
+        edit: 'edit.html'
+      }
+    }
+  }
+})
